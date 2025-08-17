65de4c344a3c37516923a0b049b23cdba62269de
vo1dy <223290528+vo1dy-lab@users.noreply.github.com>
2025-08-17 16:04:41 +0300

    feat: add a rgb random function

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ function createBoard(size = 16) {
         const htmlSquare = document.createElement("div");
         htmlSquare.style.flexBasis = `calc(100% / ${size})`;
         htmlSquare.classList.add("square");
-        htmlContainer.appendChild(htmlSquare)
+        htmlContainer.appendChild(htmlSquare);
+
+        htmlSquare.addEventListener('mouseover', (e) => {
+            e.currentTarget.style.backgroundColor = getRandomColor();
+        })
     }
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 createBoard();
